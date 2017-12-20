7a6e5b849513093b2f2fc1d253b0abac4c98b1d6
Alexander Shepetko <a@shepetko.com>
2017-12-20 20:06:19 +0200

    Minor update 2.2.1

--- __init__.py
+++ __init__.py
@@ -12,26 +12,10 @@ if _plugman.is_installed(__name__):
     from ._api import define, create, average, count, delete_all, is_flagged, total, toggle, delete, is_defined, find
 
 
-def plugin_install():
-    from plugins import auth
-
-    auth.switch_user_to_system()
-
-    # Allow ordinary users to create, modify and delete images
-    for role in auth.get_roles():
-        role.permissions = list(role.permissions) + [
-            'odm_auth.create.flag',
-            'odm_auth.delete_own.flag',
-        ]
-        role.save()
-
-    auth.restore_user()
-
-
 def plugin_load():
     from pytsite import tpl, lang, events
-    from plugins import permissions, odm, assetman, http_api
-    from . import _model, _eh, _http_api_controllers
+    from plugins import permissions, odm, assetman
+    from . import _api, _model, _eh, _http_api_controllers
 
     # Resources
     lang.register_package(__name__)
@@ -48,6 +32,18 @@ def plugin_load():
     # ODM models
     odm.register_model('flag', _model.Flag)
 
+    # Event listeners
+    events.listen('odm@entity.delete', _eh.odm_entity_delete)
+
+    # Define default flag types
+    _api.define('like')
+    _api.define('bookmark')
+
+
+def plugin_load_uwsgi():
+    from plugins import http_api
+    from . import _http_api_controllers
+
     # HTTP API handlers
     http_api.handle('GET', 'flag/count/<flag_type>/<model>/<uid>', _http_api_controllers.GetCount, 'flag@get_count')
     http_api.handle('POST', 'flag/<flag_type>/<model>/<uid>', _http_api_controllers.Post, 'flag@post')
@@ -55,9 +51,21 @@ def plugin_load():
     http_api.handle('PATCH', 'flag/<flag_type>/<model>/<uid>', _http_api_controllers.Patch, 'flag@patch')
     http_api.handle('DELETE', 'flag/<flag_type>/<model>/<uid>', _http_api_controllers.Delete, 'flag@delete')
 
-    # Event listeners
-    events.listen('odm@entity.delete', _eh.odm_entity_delete)
 
-    # Define default flag types
-    define('like')
-    define('bookmark')
+def plugin_install():
+    from plugins import auth, assetman
+
+    plugin_load()
+
+    # Allow ordinary users to create, modify and delete images
+    auth.switch_user_to_system()
+    for role in auth.get_roles():
+        role.permissions = list(role.permissions) + [
+            'odm_auth.create.flag',
+            'odm_auth.delete_own.flag',
+        ]
+        role.save()
+    auth.restore_user()
+
+    assetman.build(__name__)
+    assetman.build_translations()
