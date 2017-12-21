5e00252c6189a512bb25eecd9f241183fc4c9bb7
Alexander Shepetko <a@shepetko.com>
2017-12-21 12:38:22 +0200

    Minor update 2.2.2

--- __init__.py
+++ __init__.py
@@ -12,19 +12,45 @@ if _plugman.is_installed(__name__):
     from ._api import define, create, average, count, delete_all, is_flagged, total, toggle, delete, is_defined, find
 
 
+def _register_assetman_resources():
+    from plugins import assetman
+
+    if not assetman.is_package_registered(__name__):
+        assetman.register_package(__name__)
+        assetman.js_module('flag-widget-like', __name__ + '@js/flag-widget-like')
+        assetman.t_less(__name__)
+        assetman.t_js(__name__)
+
+    return assetman
+
+
+def plugin_install():
+    from plugins import auth
+
+    # Allow ordinary users to create, modify and delete images
+    auth.switch_user_to_system()
+    for role in auth.get_roles():
+        role.permissions = list(role.permissions) + [
+            'odm_auth.create.flag',
+            'odm_auth.delete_own.flag',
+        ]
+        role.save()
+    auth.restore_user()
+
+    assetman = _register_assetman_resources()
+    assetman.build(__name__)
+    assetman.build_translations()
+
+
 def plugin_load():
     from pytsite import tpl, lang, events
-    from plugins import permissions, odm, assetman
+    from plugins import permissions, odm
     from . import _api, _model, _eh, _http_api_controllers
 
     # Resources
     lang.register_package(__name__)
     tpl.register_package(__name__)
-
-    assetman.register_package(__name__)
-    assetman.js_module('flag-widget-like', __name__ + '@js/flag-widget-like')
-    assetman.t_less(__name__)
-    assetman.t_js(__name__)
+    _register_assetman_resources()
 
     # Permission group
     permissions.define_group('flag', 'flag@flag')
@@ -50,22 +76,3 @@ def plugin_load_uwsgi():
     http_api.handle('GET', 'flag/<flag_type>/<model>/<uid>', _http_api_controllers.Get, 'flag@get')
     http_api.handle('PATCH', 'flag/<flag_type>/<model>/<uid>', _http_api_controllers.Patch, 'flag@patch')
     http_api.handle('DELETE', 'flag/<flag_type>/<model>/<uid>', _http_api_controllers.Delete, 'flag@delete')
-
-
-def plugin_install():
-    from plugins import auth, assetman
-
-    plugin_load()
-
-    # Allow ordinary users to create, modify and delete images
-    auth.switch_user_to_system()
-    for role in auth.get_roles():
-        role.permissions = list(role.permissions) + [
-            'odm_auth.create.flag',
-            'odm_auth.delete_own.flag',
-        ]
-        role.save()
-    auth.restore_user()
-
-    assetman.build(__name__)
-    assetman.build_translations()
